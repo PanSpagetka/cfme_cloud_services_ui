martinpovolny/miq_plugin_example

Add Red Hat Cloud services plugin

app/javascript/packs/component-definitions-common.js
@@ -1,0 +1,3 @@
+import RedHatCloudServices from './red_hat_cloud_services_table'
+
+ManageIQ.component.addReact('RedHatCloudServices', RedHatCloudServices);

PanSpagetka/cfme_cloud_services_ui

app/javascript/packs/component-definitions-common.js
@@ -1,0 +1,2 @@
+import RedHatCloudServices from './red_hat_cloud_services'
+ManageIQ.component.addReact('RedHatCloudServices', RedHatCloudServices);